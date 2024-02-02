783a313f5acb6af8fe14e6915c3ce4f691353b2b
Anton Jardine <40690620@live.napier.ac.uk>
2024-02-02 14:40:55 -0400

    Updated Packaged File

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -9,20 +9,18 @@ public class App
 {
     public static void main(String[] args)
     {
-        // Connect to MongoDB on local system - we're using port 27000
-        MongoDatabase database;
-        try (MongoClient mongoClient = new MongoClient("mongo-dbserver")) {
-            // Get a database - will create when we use it
-            database = mongoClient.getDatabase("mydb");
-        }
+        // Connect to MongoDB
+        MongoClient mongoClient = new MongoClient("mongo-dbserver");
+        // Get a database - will create when we use it
+        MongoDatabase database = mongoClient.getDatabase("mydb");
         // Get a collection from the database
         MongoCollection<Document> collection = database.getCollection("test");
         // Create a document to store
         Document doc = new Document("name", "Anton Jardine")
                 .append("class", "Software Engineering Methods")
                 .append("year", "2024")
-                .append("result", new Document("CW", 95).append("EX",85));
-        /* add document to collection */
+                .append("result", new Document("CW", 95).append("EX", 85));
+        // Add document to collection
         collection.insertOne(doc);
 
         // Check document in collection
