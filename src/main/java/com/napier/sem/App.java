454d6688fa11a9e96e3fc8633c3cc9ceac5f43b1
Anton Jardine <40690620@live.napier.ac.uk>
2024-03-23 16:50:56 -0400

    Reverted to passing Code To Test Failing Build on GitHub

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -23,11 +23,15 @@ public class App {
         }
 
         int retries = 10;
+        boolean shouldWait = false;
         for (int i = 0; i < retries; ++i) {
             System.out.println("Connecting to database...");
             try {
-                // Wait a bit for db to start
-                Thread.sleep(delay);
+                if (shouldWait) {
+                    // Wait a bit for db to start
+                    Thread.sleep(delay);
+                }
+
                 // Connect to database
                 con = DriverManager.getConnection("jdbc:mysql://" + location
                                 + "/employees?allowPublicKeyRetrieval=true&useSSL=false",
@@ -35,8 +39,11 @@ public class App {
                 System.out.println("Successfully connected");
                 break;
             } catch (SQLException sqle) {
-                System.out.println("Failed to connect to database attempt " +                                  Integer.toString(i));
+                System.out.println("Failed to connect to database attempt " + i);
                 System.out.println(sqle.getMessage());
+
+                // Let's wait before attempting to reconnect
+                shouldWait = true;
             } catch (InterruptedException ie) {
                 System.out.println("Thread interrupted? Should not happen.");
             }
@@ -44,33 +51,21 @@ public class App {
     }
 
     public static void main(String[] args) {
-        // Create new Application and connect to Database
+        // Create new Application and connect to database
         App a = new App();
 
-        // Connect to database
-        if(args.length < 1){
-            a.connect("localhost:33060", 0);
-        }else{
+        if (args.length < 1) {
+            a.connect("localhost:33060", 30000);
+        } else {
             a.connect(args[0], Integer.parseInt(args[1]));
         }
 
-        // Extract employee salary information
-        //ArrayList<Employee> employees = a.getAllSalaries();
-
-        // Test the size of the returned data - should be 240124
-        //System.out.println(employees.size());
-
-        // Print employee salaries
-        //a.printSalaries(employees);
-
-        // Get Salary by Department
         Department dept = a.getDepartment("Development");
         ArrayList<Employee> employees = a.getSalariesByDepartment(dept);
 
+
         // Print salary report
         a.printSalaries(employees);
-
-        //a.displayDepartment(dept);
 
         // Disconnect from database
         a.disconnect();
@@ -144,9 +139,11 @@ public class App {
             Statement stmt = con.createStatement();
             // Create string for SQL statement
             String strSelect =
+
                     "SELECT e.emp_no, e.first_name, e.last_name "
                             + "FROM employees AS e "
                             + "WHERE e.emp_no = " + ID;
+
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
             // Return new employee if valid.
@@ -282,7 +279,5 @@ public class App {
             }
         }
     }
-
-
 
 }
