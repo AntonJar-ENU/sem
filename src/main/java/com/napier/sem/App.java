056ae953b734ab0e067074bc9bd79d7eb02f5418
Anton Jardine <40690620@live.napier.ac.uk>
2024-03-23 15:18:45 -0400

    Modified Delay to Connect to DB

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -49,7 +49,7 @@ public class App {
 
         // Connect to database
         if(args.length < 1){
-            a.connect("localhost:33060", 0);
+            a.connect("localhost:33060", 30000);
         }else{
             a.connect(args[0], Integer.parseInt(args[1]));
         }
